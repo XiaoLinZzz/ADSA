03091de8b2e436adae52ae0d5f87fe587e786ea5
XiaoLinzzz <mlj20020716@163.com>
2022-03-31 17:16:12 +0800

    main

--- assignment2/main.cpp
+++ assignment2/main.cpp
@@ -4,10 +4,37 @@ using namespace std;
 // An AVL tree node
 class Node
 {
-    public:
     int key;
     Node *left;
     Node *right;
     int height;
 };
+
+int height(Node *N)
+{
+    if (N == NULL)
+        return 0;
+    return N -> height;
+}
+
+// new node
+Node* newNode(int key)
+{
+    Node* node = new Node();
+    node->key = key;
+    node->left = NULL;
+    node->right = NULL;
+    node->height = 1; 
+
+    return(node);
+}
+
+// find node
+int 
+
+
+// Tree insert
+
+
+// Tree delete
 
